74979968011bc36a2b83bee40ab4dc7ff04c2f3d
rscheder <ralf.schederecker@ontrolware.de>
2020-01-08 14:52:43 +0100

    gcp instance added

--- tf/basic/gcp/main.tf
+++ tf/basic/gcp/main.tf
@@ -1,6 +1,33 @@
 provider "google" {
-  credentials = "${file("account.json")}"
-  project     = "alpine-tracker-251408"
-  region      = "us-central1"
-  zone        = "us-central1-c"
+  credentials = file("schedix-gcp.json")
+  project     = var.my_gcp_project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_project_metadata_item" "ssh-keys" {
+  key   = "ssh-keys"
+  value = "${var.public_key}"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "tf_linux"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
 }
---
+++ tf/basic/gcp/variables.tf
@@ -0,0 +1,15 @@
+variable "my_gcp_project" {
+  default = "alpine-tracker-251408"
+}
+
+variable "region" {
+  default = "europe-west3"
+}
+
+variable "zone" {
+  default = "europe-west3-c"
+}
+
+variable "public_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDA93UxQh005Ywjf23kzt3AeHOj+z1t1WgCjUJ2UrQh34geo6gEWw5kvY3UVKMF1I+s6H6Ua2f6mCDBgFer921peLvnS0dudr2vR0ybZhWUttnhJGteVn1vi6/HyP5Lm5aSevG0EcgemOtmUNIbpa6vMev09MPs2SJpsPJdTiRQjotnlDktvxXLwmnM1DB4YOxjrGSQqNwIVmQqOcNaKZEB3xVV2nIAjnIilRU6UtwBxdllzU+FXFVBKm0BVNaRCCmFbVg/PxA/2rUriYmtZMIl4/3oGcM/SKb3P/TCgIsh8PWaMeP4duXTqBBWntfWbk97065Sp19EtPCq22MccVD3"
+}
